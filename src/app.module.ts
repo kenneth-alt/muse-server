238c240b2e5479a7008f4035ce1287b3ef95f7ab
kenneth-alt <kenneth.ugo15@gmail.com>
2023-12-30 13:07:10 -0700

    main module files

---
+++ src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { DatabaseModule } from './database/database.module';
+import { UsersModule } from './users/users.module';
+import { EntriesModule } from './entries/entries.module';
+
+@Module({
+  imports: [AuthModule, DatabaseModule, UsersModule, EntriesModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
